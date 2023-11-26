47298e690e22f7229f355136fc5289c55a302cb7
Gabriel Schirmer <gabruel.cruz.schirmer@gmail.com>
2023-11-26 17:56:01 -0300

    interfacezita

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.funcionajava.funcionajava {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.funcionajava.funcionajava to javafx.fxml;
+    exports com.funcionajava.funcionajava;
+}
